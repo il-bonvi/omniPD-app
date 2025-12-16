62fd4296928ba2a8ef7f909020c1799b15f33f08
https://github.com/bonvicincoaching-collab <bonvicin.coaching@gmail.com>
2025-12-16 10:51:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -50,9 +50,10 @@ st.markdown(
     """
 Inserisci almeno **4 punti dati** (tra cui **sprint**) tempo (s) e potenza (W).  
 Oppure, più in basso ⬇️ puoi caricare un CSV con i dati.  
-Per avere valori di **A** inserisci **MMP oltre i 30 minuti** (opzionale). 
-<br>**⚠⚠grafici work in progress...⚠⚠**
-"""
+Per avere valori di **A** inserisci **MMP oltre i 30 minuti** (opzionale).  
+**⚠⚠grafici work in progress...⚠⚠**
+""",
+    unsafe_allow_html=True
 )
 
 # =========================
